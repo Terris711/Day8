6d5612219c3752c34f84bdedd37c794b9a306eea
Terris711 <86718823+Terris711@users.noreply.github.com>
2022-10-28 22:46:22 +0800

    Fixed

--- Sort.java
+++ Sort.java
@@ -4,18 +4,20 @@ public class Sort {
 
 	public static void main(String[] args) {
 		int[] array = {1,2,2,0,0,1,2,2,1};
-		sort(array);
+		int[] sortedArr = null;
+		
+		sort(array, sortedArr);
 		display(array);
 	}
 	
-	public static int[] sort(int[] arr)
+	public static int[] sort(int[] arr, int[] sortedArr)
     {
         for (int i = 0; i < arr.length - 1; i++) { // x n steps
             if (arr[i] > arr[i + 1]) { // x1 step
-                int temp = arr[i]; // x1 step
-                arr[i] = arr[i + 1]; // x1 step
-                arr[i + 1] = temp; // x1 step
-               
+                 int temp = arr[i];
+                 arr[i] = arr[i+1];
+                 arr[i+1] = temp;
+                 i = -1;
             }
         }
         return arr; // x1 step
